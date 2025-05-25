a457b2076e3f655cc54a957a6051b652980ab25c
esz-stone <krystalrosy.magic@outlook.com>
2025-05-25 18:19:57 +0630

    Reconnect project to GitHub

---
+++ README.md
@@ -0,0 +1,2 @@
+# windowformagic
+Web3-ready indentity dashboard for proving contributions and mining history
---
+++ index.js
@@ -0,0 +1,2 @@
+console.log("🚀 Welcome to windowformagic script!");
+console.log("✨ This is a ready-to-run Node.js script powered by Resporn.");
